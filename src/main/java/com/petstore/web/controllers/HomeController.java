a6002fd768c5a3cb2f4426851be40097bbaf7782
Fanekin Zipya <zipyadeborah@gmail.com>
2020-11-17 11:26:36 +0100

    Project setup

---
+++ src/main/java/com/petstore/web/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.petstore.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/welcome")
+    public @ResponseBody String welcomeMessage(){
+
+        return "Pet Store App";
+    }
+
+    @GetMapping("/")
+    public String displayWelcomePage(){
+        return "welcome";
+    }
+}
